61c4ad9007229c42afde826e102763fd79b3f777
dongzhiwei <2660847388@qq.com>
2022-04-10 23:02:52 +0800

    测试数据上传

--- internal/api/problem.go
+++ internal/api/problem.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"inherited/internal/services"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
-func  QueryAllProblem(ctx *gin.Context)  {
+const (
+	OJ_DATA          = "/home/judge/data"
+	OJ_ZIP_TEMP_DATA = OJ_DATA + "/tempzip"
+	SESSION_USER_KEY = "dongzhiwei"
+)
+
+func QueryAllProblem(ctx *gin.Context) {
 	problem := new(services.Problem)
 	pro_set, err := problem.QueryAllProblem()
 	if err != nil {
@@ -21,5 +30,30 @@ func  QueryAllProblem(ctx *gin.Context)  {
 	})
 
 	return
+
+}
+
+// FileUpload 上传测试数据zip
+func FileUpload(ctx *gin.Context) {
+
+	key := time.Now().String()
+	//f, h, err := this.GetFile("file")
+	zipDir := OJ_ZIP_TEMP_DATA
+	file, err := os.OpenFile(zipDir, os.O_CREATE, os.ModePerm)
+	defer file.Close()
+	if err != nil {
+		log.Fatal("[api.FileUpload]", err)
+		return
+	}
+
+	err2 := os.Mkdir(zipDir, os.ModePerm)
+	if err2 != nil {
+		//logs.Error(err2)
+	}
+
+	//err1 := this.SaveToFile("file", OJ_ZIP_TEMP_DATA+"/"+key+h.Filename)
+	//if err1 != nil {
+	//	this.JsonErr("文件上传错误", 24005, "")
+	//}
 
 }
